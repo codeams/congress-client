a5a645d86f50905a82350c73c075303c458d055d
Erick Montañez <codeams@gmail.com>
2017-03-27 03:03:47 -0600

    Add invoice form vee-validation

--- src/components/forms/Invoice-registration-form.vue
+++ src/components/forms/Invoice-registration-form.vue
@@ -12,89 +12,101 @@
       <div class='registration-form'>
         <div class='row'>
           <div class='select-field-container small-12 medium-6 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('rfc') }">
               <span>R.F.C.</span>
 
-              <input type='text' placeholder='Obligatorio'
-                v-model='invoice.rfc'>
+              <input name='rfc' type='text' placeholder='Obligatorio'
+                v-model='invoice.rfc' v-validate='"required|min:10|max:30"'>
             </label>
           </div>
 
           <div class='select-field-container small-12 medium-6 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('businessName') }">
               <span>Razón social</span>
 
-              <input type='text' placeholder='Obligatorio'
-                v-model='invoice.businessName'>
+              <input name='businessName' type='text' placeholder='Obligatorio'
+                v-model='invoice.businessName' v-validate='"required|max:40"'>
             </label>
           </div>
         </div>
 
         <div class='row'>
           <div class='text-field-container small-12 medium-6 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('office_street') }">
               <span>Calle</span>
 
-              <input type='text' placeholder='Obligatorio'
-                v-model='invoice.office.street'>
+              <input name='office_street' type='text' placeholder='Obligatorio'
+                v-model='invoice.office.street' v-validate='"required|max:20"'>
             </label>
           </div>
 
           <div class='text-field-container small-12 medium-6 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('office_neighborhood') }">
               <span>Colonia</span>
 
-              <input type='text' placeholder='Obligatorio'
-                v-model='invoice.office.neighborhood'>
+              <input name='office_neighborhood' type='text'
+                placeholder='Obligatorio' v-model='invoice.office.neighborhood'
+                v-validate='"required|max:40"'>
             </label>
           </div>
         </div>
 
         <div class='row'>
           <div class='text-field-container small-12 medium-7 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('office_externalNumber') }">
               <span>Número exterior</span>
 
-              <input type='text' placeholder='Obligatorio'
+              <input name='office_externalNumber' type='text'
+                placeholder='Obligatorio' v-validate='"required|max:20"'
                 v-model='invoice.office.externalNumber'>
             </label>
           </div>
 
           <div class='text-field-container small-12 medium-5 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('office_internalNumber') }">
               <span>Número interior</span>
 
-              <input type='text' placeholder='Opcional'
-                v-model='invoice.office.internalNumber'>
+              <input name='office_internalNumber' type='text'
+                placeholder='Opcional' v-model='invoice.office.internalNumber'
+                v-validate='"max:20"'>
             </label>
           </div>
         </div>
 
         <div class='row'>
           <div class='text-field-container small-12 medium-5 columns'>
-            <label>
-              <span>Municipio</span>
+            <label :class="{ 'error' : errors.has('office_district') }">
+              <span>Municipio / Delegación</span>
 
-              <input type='text' placeholder='Opcional'
-                v-model='invoice.office.district'>
+              <input name='office_district' type='text'
+                placeholder='Obligatorio' v-model='invoice.office.district'
+                v-validate='"required|max:40"'>
             </label>
           </div>
 
           <div class='text-field-container small-12 medium-4 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('office_state') }">
               <span>Estado</span>
 
-              <input type='text' placeholder='Obligatorio'
-              v-model='invoice.office.state'>
+              <select name='office_state' v-model='invoice.office.state'
+                v-validate='"required|max:40"'
+                :class='{ "is-default": !invoice.office.state }'>
+                <option value=''>Obligatorio</option>
+                <option v-for='state in statesList'
+                  :value='state'>
+                  {{ state }}
+                </option>
+              </select>
             </label>
           </div>
 
           <div class='text-field-container small-12 medium-3 columns'>
-            <label>
+            <label :class="{ 'error' : errors.has('office_postalCode') }">
               <span>Código postal</span>
 
-              <input type='text' placeholder='Opcional'
-                v-model='invoice.office.postalCode'>
+              <input name='office_postalCode' type='text'
+                placeholder='Obligatorio' v-model='invoice.office.postalCode'
+                v-validate='"required|digits:5"'>
             </label>
           </div>
         </div>
@@ -107,7 +119,12 @@
 
 <script>
 
+  import { find, propEq } from 'ramda'
+  import bus from '../../utils/bus'
+
   import { mapActions } from 'vuex'
+
+  import statesList from '../../assets/lists/states-list.json'
 
 
   export default {
@@ -115,15 +132,50 @@
 
     data() {
       return {
-        invoice: {}
+        invoice: {},
+        statesList: statesList
       }
+    },
+
+    mounted() {
+
+      bus.$on( 'validate', this.onValidate )
+
+      this.$watch( () => this.errors.errors, ( newValue, oldValue ) => {
+        const newErrors = newValue.filter( error =>
+          find( propEq( 'field', error.field ) )( oldValue ) === undefined
+        )
+
+        const oldErrors = oldValue.filter( error =>
+          find( propEq( 'field', error.field ) )( newValue ) === undefined
+        )
+
+        bus.$emit( 'errors-changed', newErrors, oldErrors )
+      })
+
     },
 
     created () {
       this.invoice = this.$store.state.invoice
     },
 
-    methods: mapActions ([ 'setInvoice' ]),
+    methods: {
+      onValidate() {
+        this.$validator.validateAll().then(() => {}).catch(() => {})
+
+        if ( this.errors.any() ) {
+          bus.$emit( 'errors-changed', this.errors.errors )
+        }
+      },
+
+      ...mapActions ([ 'setInvoice' ]),
+    },
+
+
+    beforeDestroy() {
+      bus.$emit( 'errors-changed', [], this.errors.errors )
+      bus.$off( 'validate', this.onValidate )
+    },
 
     watch: {
       invoice: {
@@ -136,3 +188,19 @@
   }
 
 </script>
+
+
+<style lang='scss' scoped>
+
+  .error {
+    input,
+    select {
+      border-color: red !important;
+    }
+
+    span {
+      color: red !important;
+    }
+  }
+
+</style>
