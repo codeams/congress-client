c7677443deb5b3dbf62c45cacebbe771751d1b59
Erick Montañez <codeams@gmail.com>
2017-03-27 03:36:01 -0600

    Add terms acceptance form validation

--- src/components/forms/Terms-acceptance-form.vue
+++ src/components/forms/Terms-acceptance-form.vue
@@ -3,10 +3,17 @@
 
     <div class='row align-center'>
       <div class='checkbox-container small-12 text-center columns'>
-        <label>
-          <input type='checkbox' v-model='termsAcceptance'>
+        <label :class="{ 'error' : errors.has('termsAcceptance') }">
+          <input name='termsAcceptance' type='checkbox'
+            v-model='termsAcceptance' v-validate='"required"'>
           He leído y estoy de acuerdo con el
           <a href='#'>aviso de privacidad</a>.
+
+          <div v-show='errors.has("termsAcceptance")'>
+            <span class='error-message'>
+              Para poder continuar es necesario aceptar los términos.
+            </span>
+          </div>
         </label>
       </div>
     </div>
@@ -17,6 +24,9 @@
 
 <script>
 
+  import { find, propEq } from 'ramda'
+  import bus from '../../utils/bus'
+
   import { mapActions } from 'vuex'
 
   export default {
@@ -26,8 +36,43 @@
       return { termsAcceptance: false }
     },
 
+    mounted() {
+
+      bus.$on( 'validate', this.onValidate )
+
+      this.$watch( () => this.errors.errors, ( newValue, oldValue ) => {
+        const newErrors = newValue.filter( error =>
+          find( propEq( 'field', error.field ) )( oldValue ) === undefined
+        )
+
+        const oldErrors = oldValue.filter( error =>
+          find( propEq( 'field', error.field ) )( newValue ) === undefined
+        )
+
+        bus.$emit( 'errors-changed', newErrors, oldErrors )
+      })
+
+    },
+
     created () {
       this.termsAcceptance = this.$store.state.termsAcceptance
+    },
+
+    methods: {
+      onValidate() {
+        this.$validator.validateAll().then(() => {}).catch(() => {})
+
+        if ( this.errors.any() ) {
+          bus.$emit( 'errors-changed', this.errors.errors )
+        }
+      },
+
+      ...mapActions ([ 'setTermsAcceptance' ])
+    },
+
+    beforeDestroy() {
+      bus.$emit( 'errors-changed', [], this.errors.errors )
+      bus.$off( 'validate', this.onValidate )
     },
 
     watch: {
@@ -35,14 +80,26 @@
         this.setTermsAcceptance( termsAcceptance )
       }
     },
-
-    methods: mapActions ([ 'setTermsAcceptance' ])
   }
 </script>
 
 
-<style lang='css'>
+<style lang='css' scoped>
+
   #terms-acceptance-form {
     margin-bottom: 60px;
   }
+
+  .checkbox-container label {
+    transition: background-color .25s ease-in-out;
+  }
+
+  .error {
+    background-color: rgba(255, 0, 0, 0.05);
+  }
+
+  .error-message {
+    color: red !important;
+  }
+
 </style>
