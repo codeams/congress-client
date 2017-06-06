7614a33e2235de005c8e53e859ac87f5e0e2b1e0
Erick A. Montañez <codeams@gmail.com>
2017-06-05 23:18:24 -0500

    Add instructions text

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -3,47 +3,29 @@
 
     <div class='row'>
       <div class='small-12 columns'>
-        <span class='section-title'>Requisitos previos al registro</span>
+        <span class='section-title'>Instrucciones</span>
       </div>
     </div>
 
     <div class='section-content'>
       <div class='text row'>
         <div class='small-12 columns'>
-          <p>
-            1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-          <p>
-            2. Morbi viverra posuere orci, sit amet pellentesque orci
-            pellentesque at. Aenean massa quam, tristique et feugiat non,
-            facilisis vitae sem. Proin vulputate feugiat velit, et convallis
-            eros. Proin vulputate rhoncus ligula id tincidunt. Maecenas
-            ornare venenatis mi finibus gravida. Fusce in nunc rhoncus,
-            rutrum enim nec, bibendum sem.
-          </p>
-          <p>
-            3. Maecenas vehicula porttitor arcu a finibus. Mauris nibh tellus,
-             facilisis nec justo eget, malesuada tempus quam. Donec vel
-            hendrerit felis, at sodales dui. Suspendisse ut mauris at ipsum
-            bibendum tempor at vel erat. Mauris gravida tincidunt lorem ac
-            dapibus. Vestibulum sit amet nisl mauris.
-          </p>
-          <p>
-            4. Integer egestas, nisi bibendum eleifend blandit, metus mauris
-            feugiat sem, id maximus dui leo sed libero. Donec at tellus ut
-            orci imperdiet bibendum vel mattis leo. Suspendisse potenti.
-            Vivamus viverra egestas arcu ut pellentesque. Donec molestie velit
-             non ullamcorper iaculis. Cras interdum leo vitae sapien pretium
-             laoreet.
-          </p>
-          <p>
-            5. Pellentesque ornare dolor vel massa euismod rhoncus.
-            Suspendisse bibendum, magna non mollis porta, lorem purus
-            sollicitudin nunc, vitae dictum velit dolor scelerisque lorem.
-            Praesent posuere libero id nisl malesuada suscipit. Nam molestie
-            turpis at luctus facilisis. Nullam luctus lacinia sem et viverra.
-            Praesent eget vestibulum ex, at euismod nibh.
-          </p>
+          <p><strong>Notas importantes:</strong></p>
+          <ul>
+            <li>Para registrarse en este sistema se requieren sus datos de depósito, no podrá registrarse hasta haber realizado el depósito correspondiente de acuerdo a las instrucciones proporcionadas.</li>
+            <li>Ninguna información se enviará ni se guardará hasta el último paso al presionar el botón “Realizar registro”.</li>
+          </ul>
+
+          <p><strong>Instrucciones:</strong></p>
+          <ul>
+            <li>Consulte la cuota de recuperación vigente.</li>
+            <li>Realice el depósito de acuerdo a las instrucciones proporcionadas.</li>
+            <li>Sólo después de haber completado los pasos anteriores regístrese utilizando este sistema.</li>
+            <li>Una vez realizado su registro recibirá un número de folio en su dirección de correo electrónico especificado.</li>
+            <li>Deberá presentarse al congreso con la ficha ORIGINAL del depósito bancario y con el formato que recibirá por correo electrónico.</li>
+            <li>No es posible modificar la información proporcionada una vez que el registro ha concluido, cualquier aclaración o modificación deberá realizarse hasta llegar al congreso.</li>
+          </ul>
+
         </div>
       </div>
     </div>
