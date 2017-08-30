1a220680f85d07676a52091d83aa7adf1356aa3a
Erick A. Montañez <codeams@gmail.com>
2017-08-30 09:18:36 -0500

    Update api url

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://congress.api.app/"',
+  API_URL: '"http://congress.codeams.me/api/v1/"',
   SAVE_PATH: '"index.php"',
   UPLOAD_IMAGE_PATH: '"upload_image.php"',
   CONFIRM_EMAIL_PATH: '"validate_email.php"'
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://congress.api.app/"',
+  API_URL: '"https://congress.codeams.me/api/v1/"',
   SAVE_PATH: '"index.php"',
   UPLOAD_IMAGE_PATH: '"upload_image.php"',
   CONFIRM_EMAIL_PATH: '"validate_email.php"',
